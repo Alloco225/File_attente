13437200dea924dd511b4cdcac6cbfe688c83af3
Hosanna Amane <amane.hosanna@jool-international.com>
2022-02-26 16:27:29 +0000

    added ClientForm

---
+++ file_attente_front/src/views/ClientForm.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        Client Form
+
+
+        <div class="bg-gray-100">
+            <input type="text" placeholder="Nom Client">
+            <select name="demande" id="">
+                <option value="">Demande 1</option>
+                <option value="">Demande 2</option>
+                <option value="">Demande 3</option>
+            </select>
+
+            <textarea name="details" id="" cols="30" rows="10" placeholder="Details demande"></textarea>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'ClientForm'
+    ,
+    data(){
+        return {
+            nom,
+            id_demande,
+            details
+        }
+    },
+    methods: {
+        async createNewClient(){
+            const dataClient = {nom: this.nom, id_demande :this.id_demande, details: this.details}
+
+            // axios
+            
+
+        }
+    }
+}
+</script>
